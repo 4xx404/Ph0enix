3266b7dc0bb5de2ea517eadc8a446d63c6ec38af
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-11-30 21:55:08 +0000

    Update ph0enix.py

--- ph0enix.py
+++ ph0enix.py
@@ -1,75 +1,85 @@
 # -*- coding: utf-8 -*-
-
-import os
+import sys
+sys.dont_write_bytecode = True
 
 from Core.Styling.Banners import sd
-Banner, sBan, eBan, iBan = sd.logo, sd.sBan, sd.eBan, sd.iBan
 from Core.Styling.Colors import bc
 
 from Core.Config import CoreConfig
-Config = CoreConfig()
+from Core.Commands import Command
+from Core.Input import InputManager
 from Core.Validity import Validation
-Validator = Validation()
 from Core.Requester import RequestHandler
-Request = RequestHandler()
 from Core.Error import ErrorHandler
-Error = ErrorHandler()
 
-def Clear():
-	os.system("clear")
-	print(Banner)
+class Ph0enix:
+	def __init__(self) -> None:
+		self.Config = CoreConfig()
+		self.Cmd = Command()
+		self.Validator = Validation()
+		self.Request = RequestHandler()
+		self.Error = ErrorHandler()
 
-def Ph0enix(LinkList, Username):
-	FoundCount = 0
-	Matches = []
+	def BuildLinks(self, Username: str):
+		self.Username:str = Username
 
-	try:
-		print(f"{sBan} Searching: {bc.GC}{Username}{bc.BC}\n")
-		for Link in LinkList:
-			Website = Validator.GetWebsiteName(Link)
-			IsInSite = Request.Search(Link, Username)
+		self.BuiltLinks = self.Validator.LinkFormatter(self.Username)
+		if(len(self.BuiltLinks) > 0):
+			return self.BuiltLinks
+		else:
+			self.Cmd.Clear()
+			print(f"{sd.eBan} No data found")
+			Initiate()
 
-			if(IsInSite):
-				FoundCount += 1
-				Matches.append(Link)
-				print(f" | Website: {bc.GC}{Website}{bc.BC}")
-				print(f" | Status: {bc.GC}Match Found{bc.BC}")
-				print(f" | Location: {bc.GC}{Link}{bc.BC}\n")
+	def StartSearch(self, Username, FormattedLinks: list):
+		self.Username = Username
+		self.Links: list = FormattedLinks
+
+		self.FoundCount = 0
+		self.Matches = []
+
+		print(f"{sd.sBan} Searching for {bc.GC}{Username}{bc.BC}\n")
+		for self.Link in self.Links:
+			self.Website = self.Validator.GetWebsiteName(self.Link)
+			self.IsInSite = self.Request.Search(self.Link, self.Username)
+
+			if(self.IsInSite):
+				self.FoundCount += 1
+				self.Matches.append(self.Link)
+				print(f" | Website: {bc.GC}{self.Website}{bc.BC}\n | Status: {bc.GC}Match Found{bc.BC}\n | Location: {bc.GC}{self.Link}{bc.BC}\n")
 			else:
-				print(f" | Website: {bc.RC}{Website}{bc.BC}")
-				print(f" | Status: {bc.RC}Not Found{bc.BC}")
-				print(f" | Location: {bc.RC}{Link}{bc.BC}\n")
+				print(f" | Website: {bc.RC}{self.Website}{bc.BC}\n | Status: {bc.RC}Not Found{bc.BC}\n | Location: {bc.RC}{self.Link}{bc.BC}\n")
 
-		Clear()
+		return self.Matches, self.FoundCount
 
-		print(f"{sBan} Found {bc.GC}{str(FoundCount)}{bc.BC} matches out of {bc.GC}{str(len(LinkList))}{bc.BC} websites\n")
-		for Match in Matches:
-			print(f" | Website: {bc.GC}{Validator.GetWebsiteName(Match)}{bc.BC}")
-			print(f" | Location: {bc.GC}{Match}{bc.BC}\n")
+	def PrintResults(self, Results: list, FoundNum: int, TotalLinkCount: int):
+		self.Cmd.Clear()
+		self.Results: list = Results
+		self.FoundCount = FoundNum
+		self.TotalLinkCount = TotalLinkCount
 
-		Initiate()
-	except KeyboardInterrupt:
-		print(f"{bc.BC} | Scan stopped...\n")
-		Initiate()
+		print(f"{sd.sBan} Found {bc.GC}{str(self.FoundCount)}{bc.BC} matches out of {bc.GC}{self.TotalLinkCount}{bc.BC} websites\n")
+		for self.Match in self.Results:
+			print(f" | Website: {bc.GC}{self.Validator.GetWebsiteName(self.Match)}{bc.BC}")
+			print(f" | Location: {bc.GC}{self.Match}{bc.BC}\n")
+
+		Initiate(True)
 
 if __name__ == '__main__':
-	Clear()
-	print(f"{iBan} Matches may not always be specific to the user you are looking for")
-	print(f"{iBan} For example, someone else could have used the same username elsewhere\n")
-
-	def Initiate():
+	def Initiate(SeenIntro = False):
 		try:
-			Username = str(input(f"{bc.BC} Username: {bc.GC}"))
+			Run = Ph0enix()
 
-			LinkBuild = Validator.LinkFormatter(Username)
-			if(LinkBuild["success"]):
-				Clear()
-				Ph0enix(LinkBuild["data"], Username.lower())
-			else:
-				Clear()
-				print(LinkBuild["data"][0]) # Print Error
-				Initiate()
+			if(not SeenIntro):
+				print(f"{sd.iBan} Matches may not always be specific to the user you are looking for")
+				print(f"{sd.iBan} For example, someone else could have used the same username elsewhere\n")
+
+			Username = InputManager().SetUsername()
+			WebsiteLinks = Run.BuildLinks(Username)
+			SearchResults, FoundCount = Run.StartSearch(Username, WebsiteLinks)
+			Run.PrintResults(SearchResults, FoundCount, len(WebsiteLinks))
 		except KeyboardInterrupt:
 			quit()
 
+	Command().Clear()
 	Initiate()
