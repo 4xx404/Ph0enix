3e98a3d2185de0f9320d6096fff9a200fdabe39d
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-11-30 21:57:46 +0000

    Update Banners.py

--- Core/Styling/Banners.py
+++ Core/Styling/Banners.py
@@ -1,21 +1,20 @@
+import emojis
 from Core.Styling.Colors import bc
 
 class sd:
-    Tick = u'\u2713'
-    Cross = u'\u2717'
-    iBan = f"{bc.BC} |{bc.GC}i{bc.BC}|"
-    sBan = f"{bc.BC} |{bc.GC}{Tick}{bc.BC}|"
-    eBan = f"{bc.BC} |{bc.RC}{Cross}{bc.BC}|{bc.RC} ERROR:{bc.BC}"
+    iBan: str = emojis.encode(f" :information_source:{bc.BC} ")
+    sBan: str = emojis.encode(f" :white_check_mark:{bc.BC} ")
+    eBan: str = emojis.encode(f" :x:{bc.RC} ERROR:{bc.BC}")
 
     Author = f"{bc.BC}\n\t| Author:{bc.RC} 4{bc.GC}x{bc.BC}x{bc.RC}4{bc.GC}0{bc.BC}4 \t\t     |\n"
     Version = f"{bc.BC}\t| Version:{bc.RC} 3{bc.GC}.{bc.BC}0 \t\t\t     |\n"
     Github = f"{bc.BC}\t| Github:{bc.RC} h{bc.GC}t{bc.BC}t{bc.RC}p{bc.GC}s{bc.BC}:{bc.RC}/{bc.GC}/{bc.BC}g{bc.RC}i{bc.GC}t{bc.BC}h{bc.RC}u{bc.GC}b{bc.BC}.{bc.RC}c{bc.GC}o{bc.BC}m{bc.RC}/{bc.GC}4{bc.BC}x{bc.RC}x{bc.GC}4{bc.BC}0{bc.RC}4{bc.GC}/ {bc.BC}|\n"
 
-    logo = bc.BC + '''
+    Logo = f'''{bc.BC}
     .------..------..------..------..------..------..------..------..------.
-    |'''+bc.GC+'''P'''+bc.BC+'''.--. ||'''+bc.GC+'''H'''+bc.BC+'''.--. ||'''+bc.GC+'''0'''+bc.BC+'''.--. ||'''+bc.GC+'''E'''+bc.BC+'''.--. ||'''+bc.GC+'''N'''+bc.BC+'''.--. ||'''+bc.GC+'''I'''+bc.BC+'''.--. ||'''+bc.GC+'''X'''+bc.BC+'''.--. ||'''+bc.GC+'''V'''+bc.BC+'''.--. ||'''+bc.GC+'''3'''+bc.BC+'''.--. |
+    |{bc.GC}P{bc.BC}.--. ||{bc.GC}H{bc.BC}.--. ||{bc.GC}0{bc.BC}.--. ||{bc.GC}E{bc.BC}.--. ||{bc.GC}N{bc.BC}.--. ||{bc.GC}I{bc.BC}.--. ||{bc.GC}X{bc.BC}.--. ||{bc.GC}V{bc.BC}.--. ||{bc.GC}3{bc.BC}.--. |
     | :/\: || :/\: || :/\: || (\/) || :(): || (\/) || :/\: || :(): || :/\: |
     | (__) || (__) || :\/: || :\/: || ()() || :\/: || (__) || ()() || (__) |
-    | "--"'''+bc.GC+'''P'''+bc.BC+'''|| "--"'''+bc.GC+'''H'''+bc.BC+'''|| "--"'''+bc.GC+'''0'''+bc.BC+'''|| "--"'''+bc.GC+'''E'''+bc.BC+'''|| "--"'''+bc.GC+'''N'''+bc.BC+'''|| "--"'''+bc.GC+'''I'''+bc.BC+'''|| "--"'''+bc.GC+'''X'''+bc.BC+'''|| "--"'''+bc.GC+'''V'''+bc.BC+'''|| "--"'''+bc.GC+'''2'''+bc.BC+'''|
+    | "--"{bc.GC}P{bc.BC}|| "--"{bc.GC}H{bc.BC}|| "--"{bc.GC}0{bc.BC}|| "--"{bc.GC}E{bc.BC}|| "--"{bc.GC}N{bc.BC}|| "--"{bc.GC}I{bc.BC}|| "--"{bc.GC}X{bc.BC}|| "--"{bc.GC}V{bc.BC}|| "--"{bc.GC}2{bc.BC}|
     `------'`------'`------'`------'`------'`------'`------'`------'`------'
-    ''' + Author + Version + Github
+    {Author}{Version}{Github}'''
