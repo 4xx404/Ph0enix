c0e205a31f0f7642f69d8ef51e4b6443382ad834
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-11-30 21:55:35 +0000

    Update Validity.py

--- Core/Validity.py
+++ Core/Validity.py
@@ -1,3 +1,4 @@
+from typing import List
 from tld import get_tld
 
 from Core.Config import CoreConfig
@@ -8,25 +9,10 @@ class Validation:
         self.Config = CoreConfig()
         self.Error = ErrorHandler()
 
-    def NotEmpty(self, Object):
-        self.Object = Object
-
-        if(type(self.Object) == list):
-            if(len(self.Object) > 0):
-                return True
-            else:
-                return False
-        elif(type(self.Object) == str):
-            if(self.Object != "" or self.Object != " "):
-                return True
-            else:
-                return False
-
     def GetWebsiteName(self, Link):
         self.Link = Link
-        self.WebsiteName = get_tld(self.Link, as_object=True)
+        self.Site = get_tld(self.Link, as_object=True).domain.title()
 
-        self.Site = self.WebsiteName.domain.title()
         if(self.Site == "Steamcommunity"):
             self.Site = "Steam"
         else:
@@ -34,25 +20,15 @@ class Validation:
         
         return self.Site
 
-    def LinkFormatter(self, Username):
-        self.ResponsePack = {
-            "success": True,
-            "data": []
-        }
+    def LinkFormatter(self, Username: str):
+        self.Username: str = Username
+        self.Links: list = self.Config.SitePack
 
-        if(self.NotEmpty(Username)):
-            self.Username = Username
-        else:
-            self.ResponsePack["success"] = False
-            self.ResponsePack["data"].append(self.Error.Throw("empty_username_value", None))
-
-        self.Links = self.Config.LinkList
+        self.Response: list = []
         for self.Link in self.Links:
             if("[USER]" in self.Link):
-                self.ResponsePack["data"].append(self.Link.split(":", 1)[1].replace("[USER]", self.Username))
+                self.Response.append(self.Link.split(":", 1)[1].replace("[USER]", self.Username))
             else:
-                self.ResponsePack["success"] = False
-                self.ResponsePack["data"].append(self.Error.Throw("link_username_replacement_chunk_not_found", self.Username))
-                return self.ResponsePack
+                print(self.Error.Throw("link_username_replacement_chunk_not_found", self.Username))
 
-        return self.ResponsePack
+        return self.Response
