8c1bd9bbd719de134f5b9226337d699e5a27ab91
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-11-30 21:56:41 +0000

    Update Error.py

--- Core/Error.py
+++ Core/Error.py
@@ -1,12 +1,11 @@
 from Core.Styling.Banners import sd
-eBan = sd.eBan
 from Core.Styling.Colors import bc
 
 class ErrorHandler:
     def __init__(self):
         self.DefinedErrors = [
             "empty_username_value",
-            "build_links_failed",
+            "username_length_too_short",
             "link_username_replacement_chunk_not_found",
             "none_200_response",
         ]
@@ -17,15 +16,15 @@ class ErrorHandler:
 
         if(self.ErrorType in self.DefinedErrors):
             if(self.ErrorType == "empty_username_value"):
-                return f"\n{eBan} Username value cannot be empty\n"
+                return f"\n{sd.eBan} Username value cannot be empty\n"
+            elif(self.ErrorType == "username_length_too_short"):
+                return f"\n{sd.eBan} Invalid Username {bc.RC}{self.ErrorData}{bc.BC}, length must be more than 1\n"
 
-            elif(self.ErrorType == "build_links_failed"):
-                return f"\n{eBan} Failed to build link list\n"
             elif(self.ErrorType == "link_username_replacement_chunk_not_found"):
-                return f"\n{eBan} Link {bc.RC}{self.ErrorData}{bc.BC} does not include {bc.RC}\"[USER]\"{bc.BC} to replace the username\n"
+                return f"\n{sd.eBan} Link {bc.RC}{self.ErrorData}{bc.BC} does not include {bc.RC}\"[USER]\"{bc.BC} to replace the username\n"
 
             elif(self.ErrorType == "none_200_response"):
-                return f"\n{eBan} Got no response from {bc.RC}{self.ErrorData}{bc.BC}\n"       
+                return f"\n{sd.eBan} Got no response from {bc.RC}{self.ErrorData}{bc.BC}\n"       
 
         else:
-            return f"\n{eBan} Undefined Error Type\n"
+            return f"\n{sd.eBan} Undefined Error Type {bc.RC}{self.ErrorType}{bc.BC}\n"
